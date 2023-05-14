6692dba6cc1cfb067a09d2a67d20e6951a859646
Mahdi Hasanzadeh <Mahdi.h2619@gmail.com>
2023-05-14 16:49:56 +0430

    server added

--- server.js
+++ server.js
@@ -1,3 +1,13 @@
-console.log("mahdi");
-console.log("hamid");
-console.log("ali");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("res");
+  if (req.url === "/") {
+    res.write("Mahdi Hasanzadeh, ID: 123435");
+    res.end();
+  }
+});
+
+server.listen(4000, () => {
+  console.log("sever is listening on port 4000");
+});
